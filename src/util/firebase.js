42785959b011329b68a11bf72d6474b966de0437
Hilton Shumway <hillshum@gmail.com>
2017-11-06 22:23:13 -0700

    Tweak Firebase config for sharing
    use a separate file that's not under version control for the secrets

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase'
-const firebaseConfig = {
-  apiKey: "AIzaSyD0DH7UPPGYJuuSNoLZmVnfpYnSbCwB_r0",
-  authDomain: "spyfall-127c6.firebaseapp.com",
-  databaseURL: "https://spyfall-127c6.firebaseio.com",
-  projectId: "spyfall-127c6",
-  storageBucket: "spyfall-127c6.appspot.com",
-  messagingSenderId: "707568045960"
-}
+
+import firebaseConfig from '../config/firebase'
 
 firebase.initializeApp(firebaseConfig)
 
